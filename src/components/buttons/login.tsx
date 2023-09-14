f5e1eeb66a0a7b8559ca95c57d44377d76ca2ad8
Matheus Pini <43689307+MatheusPini@users.noreply.github.com>
2023-09-14 11:12:21 -0400

    feat: implementando possibilidade de adicionar icone no componente de button

--- src/components/buttons/login.tsx
+++ src/components/buttons/login.tsx
@@ -1,9 +1,13 @@
 interface ILoginButton {
     label: string
     type: "button" | "submit" | "reset" | undefined
+    icon?: React.ReactNode
 }
-export const LoginButton = ({ label, type = 'submit' }: ILoginButton) => {
+export const LoginButton = ({ label, type = 'submit', icon = null }: ILoginButton) => {
     return (
-        <button type={type} className="flex w-full justify-center rounded-md bg-gray-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-gray-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-600">{label}</button>
+        <button type={type} className="flex w-full items-center justify-center rounded-md bg-gray-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-gray-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-600">
+            {label}
+            {icon && icon}
+        </button>
     )
 }
